cf8b5bcd6fe9c90fa1f28adbd61bde5c835f53d9
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2023-11-02 14:56:09 -0400

    feat(core): generates the project
    Added OpenID Connect authentication using the `react-oidc-context` library in the Next.js application. The OpenID Connect configuration introduced includes client ID, redirect URI, scope, and authority. Code settings for ESLint and Prettier was also configured along with adding their configurations to several .xml files. The unnecessary "hello" API route was removed. The home page now displays different messages based on the user's authentication status.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from 'react-oidc-context';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const oidcConfig = {
+    client_id: 'pres',
+    redirect_uri: 'https://localhost:3000/authentication/callback',
+    scope: 'openid profile email api offline_access',
+    authority: 'https://auth.ncats.nih.gov/_api/v2/auth/NCI-CCR-TEST',
+};
+
+export default function App({Component, pageProps}: AppProps) {
+    return <AuthProvider {...oidcConfig}>
+        <Component {...pageProps} />
+    </AuthProvider>
 }
